8a5de45c243173b24226f56a03549a4417f3881c
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-03-09 16:25:30 -0300

    changed favicon for the same element in the homepage

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: "standalone",
-        icon: "static/favicon.svg",
+        icon: "src/images/favicon.png",
       },
     },
     {
