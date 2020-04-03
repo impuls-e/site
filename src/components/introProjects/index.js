02e2933440df427437b37a794f4ab714b8919469
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-03 17:41:32 -0300

    Create html from container

---
+++ src/components/introProjects/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import "./styles.css"
+
+export default function introProjects() {
+  return (
+    <div className="intro-projects">
+      <section>
+        <h1>Sobre o Projeto</h1>
+        <p>
+          A Gelux considera importante manter um canal de contato duradouro e de
+          fácil acesso para se comunicar com seus clientes. Foi pensando também
+          em fortalecer a imagem da empresa e disponibilizar informações
+          importantes para seus clientes que a Gelux quer o desenvolvimento do
+          seu site institucional.
+        </p>
+      </section>
+      <aside>
+        <h2>Cliente</h2>
+        <strong>Gelux</strong>
+        <h2>Serviços</h2>
+        <strong>
+          Analytics & Insight - SEO - Velocidade - Google Business - SSL -
+          Hospedagem
+        </strong>
+        <h2>Website</h2>
+        <strong>
+          <a href="https://geluxeletrica.com.br">Geluxeletrica.com.br</a>
+        </strong>
+      </aside>
+    </div>
+  )
+}
--- src/templates/project.js
+++ src/templates/project.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Header from "../components/header"
+import IntroProjects from "../components/introProjects"
 import Contact from "../components/contact"
 
 const ProjectTemplate = ({ pageContext }) => {
@@ -10,8 +11,7 @@ const ProjectTemplate = ({ pageContext }) => {
     <Layout>
       <Seo />
       <Header />
-      <h1>{project.title}</h1>
-      <div>{project.description}</div>
+      <IntroProjects />
       <Contact />
     </Layout>
   )
