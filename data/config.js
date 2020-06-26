388dad0f2c0111edf557dc753b3583001ce07786
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-06-26 15:17:23 -0300

    Fix small bugs in SEO component

--- data/config.js
+++ data/config.js
@@ -1,9 +1,9 @@
 const config = {
-  siteTitle: "Impuls-e - Sites Inteligentes",
+  siteTitle: "Impuls-e - Sites e Soluções inteligentes.",
   siteDescription:
     "Conceitos de design com inovação digital para criar plataformas web e mobile fáceis de usar e que trazem resultados.",
   siteTitleShort: "IP",
-  siteUrl: "http://impulse.works",
+  siteUrl: "http://impuls-e.works",
   siteImage: `${__dirname}/../src/images/logo-rocket256.png`,
   siteIcon: `${__dirname}/../src/images/logo-rocket.png`,
   siteRss: "/rss.xml",
