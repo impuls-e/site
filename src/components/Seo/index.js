d714a9f9ac8399f29f1e9fa50255a89dd936b9af
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-06-26 15:36:52 -0300

    update SEO component

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -18,66 +18,55 @@ const SEO = () => (
     }) => {
       const image = `${siteUrl}${siteImage}`
       return (
-          <Helmet title={siteTitle} descriptionTemplate={siteDescription}>
-            <meta name="description" content={siteDescription} />
-            <meta name="image" content={image} />
-            <meta charSet="utf-8" />
+        <Helmet title={siteTitle} descriptionTemplate={siteDescription}>
+          <meta name="description" content={siteDescription} />
+          <meta name="image" content={image} />
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          ></meta>
+          <meta
+            name="google-site-verification"
+            content="3rrVojyccX1AnT_5-84qWDzn3S3NAvs2c6OvqgOLFYs"
+          />
+          {siteUrl && <meta property="og:url" content={siteUrl} />}
+
+          <meta property="og:type" content="website" />
+
+          {siteTitle && <meta property="og:title" content={siteTitle} />}
+          {siteDescription && (
+            <meta property="og:description" content={siteDescription} />
+          )}
+          {image && (
             <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1.0"
-            ></meta>
-            <meta name="google-site-verification" content="3rrVojyccX1AnT_5-84qWDzn3S3NAvs2c6OvqgOLFYs" />
-            {siteUrl && <meta property="og:url" content={siteUrl} />}
+              property="og:image"
+              content="https://impuls-e.works/static/d1810ab9eeabf17a1fec022a86a8546b/69585/logo.png"
+            />
+          )}
 
-            <meta property="og:type" content="website" />
+          {image && (
+            <meta
+              property="og:image:url"
+              content="https://impuls-e.works/static/d1810ab9eeabf17a1fec022a86a8546b/69585/logo.png"
+            />
+          )}
 
-            {siteTitle && <meta property="og:title" content={siteTitle} />}
-            {siteDescription && (
-              <meta property="og:description" content={siteDescription} />
-            )}
-            {image && (
-              <meta
-                property="og:image"
-                content="https://impuls-e.works/static/d1810ab9eeabf17a1fec022a86a8546b/69585/logo.png"
-              />
-            )}
-
-            {image && (
-              <meta
-                property="og:image:url"
-                content="https://impuls-e.works/static/d1810ab9eeabf17a1fec022a86a8546b/69585/logo.png"
-              />
-            )}
-<<<<<<< HEAD
-            {seo.image && <meta property="og:image:width" content="200" />}
-            {seo.image && <meta property="og:image:height" content="276" />}
-
-            <meta name="twitter:site" content="https://twitter.com" />
+          {image && <meta property="og:image:type" content="image/jpeg" />}
+          {image && <meta name="twitter:site" content="@impulsewks"></meta>}
+          <meta name="twitter:card" content="summary_large_image" />
+          {siteTwitter && <meta name="twitter:creator" content={siteTwitter} />}
+          {siteTitle && <meta name="twitter:title" content={siteTitle} />}
+          {siteDescription && (
+            <meta name="twitter:description" content={siteDescription} />
+          )}
+          {image && (
             <meta
               name="twitter:image"
-              content="https://impuls-e.works/static/d1810ab9eeabf17a1fec022a86a8546b/61e93/logo.webp"
+              content="https://impuls-e.works/static/d1810ab9eeabf17a1fec022a86a8546b/69585/logo.png"
             />
-=======
-
-            {image && <meta property="og:image:type" content="image/jpeg" />}
-            {image && <meta name="twitter:site" content="@impulsewks"></meta>}
->>>>>>> c9966bcd2ad152abe2c261089dbf7c6017b73ef4
-            <meta name="twitter:card" content="summary_large_image" />
-            {siteTwitter && (
-              <meta name="twitter:creator" content={siteTwitter} />
-            )}
-            {siteTitle && <meta name="twitter:title" content={siteTitle} />}
-            {siteDescription && (
-              <meta name="twitter:description" content={siteDescription} />
-            )}
-            {image && (
-              <meta
-                name="twitter:image"
-                content="https://impuls-e.works/static/d1810ab9eeabf17a1fec022a86a8546b/69585/logo.png"
-              />
-            )}
-          </Helmet>
-        </>
+          )}
+        </Helmet>
       )
     }}
   />
